f9e987bc99b15e3fe0b865858e4f0f4aff128408
Aaron <a.genevision14@gmail.com>
2022-03-20 20:43:59 -0500

    added font-awesome to directory, created and completed split screen slider project

---
+++ web-development/js/slider.js
@@ -0,0 +1,32 @@
+const sliderBackdrop = document.querySelector('.backdrop');
+const rightSlide = document.querySelector('.right-slide');
+const leftSlide = document.querySelector('.left-slide');
+const upBtn = document.querySelector('.up-btn');
+const downBtn = document.querySelector('.down-btn');
+const slidesLength = rightSlide.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+leftSlide.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => changeSlide('up'));
+downBtn.addEventListener('click', () => changeSlide('down'));
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderBackdrop.clientHeight;
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0;
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1;
+        }
+    }
+
+    rightSlide.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    leftSlide.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+
+}
